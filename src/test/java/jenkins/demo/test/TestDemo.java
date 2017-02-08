3e889aeca7eab4cd78e0005034d4ca0f5b96d056
chanhlt <chanhlt@ubuntu16.04>
2017-02-08 16:58:32 +0700

    commit

---
+++ src/test/java/jenkins/demo/test/TestDemo.java
@@ -0,0 +1,21 @@
+package jenkins.demo.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by chanhlt on 08/02/2017.
+ */
+public class TestDemo {
+    @Test
+    public void test01(){
+        System.out.println("OK 01 ");
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("OK 02 ");
+        assertTrue(false);
+    }
+
+}
